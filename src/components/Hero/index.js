57b8c3f9e95b14fed82141be96ba26d394c0db55
Rayane Reis <rayanereismota2@gmail.com>
2024-06-24 23:22:41 -0300

    feat: Add responsive Hero Component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,12 +4,12 @@ import "./Hero.css";
 export default function Hero() {
     return (
         <section className="hero">
-            <div className="hero-text-container">
-                <h2>O céu é o limite com nossa <span className="hero-text-emphasis">impressão 3D</span></h2>
-                <h3>Transformamos sua ideia em produtos com tecnologia 3d de ponta.</h3>
-                <button>Peça um orçamento</button>
+            <div className="hero__text-container">
+                <h2 className="hero__title">O céu é o limite com nossa <span className="hero__text-emphasis">impressão 3D</span></h2>
+                <h3 className="hero__text">Transformamos sua ideia em produtos com tecnologia 3d de ponta.</h3>
+                <button className="hero__button">Peça um orçamento</button>
             </div>
-            <img src={HeroArt} alt=""/>
+            <img src={HeroArt} alt="" className="hero__figure"/>
         </section>
     );
 }
